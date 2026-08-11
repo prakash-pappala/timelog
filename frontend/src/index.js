7c05bd6faf2ece864954ee0edc96f78dbe5d6968
prakash <prakashsenior3@gmail.com>
2026-08-11 12:57:01 -0500

    remove conflicting index.js

--- frontend/src/index.js
+++
@@ -1,7 +0,0 @@
-if ('serviceWorker' in navigator) {
-  window.addEventListener('load', () => {
-    navigator.serviceWorker.register('/sw.js')
-      .then(reg => console.log('Service Worker registered'))
-      .catch(err => console.log('Service Worker error:', err))
-  })
-}
