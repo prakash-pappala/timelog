a10ed960f30c52c80b11c87b63833cbc23d5bc11
prakash <prakashsenior3@gmail.com>
2026-08-11 13:52:40 -0500

    add caching logic to service worker

--- frontend/public/sw.js
+++ frontend/public/sw.js
@@ -1,19 +1,36 @@
-const CACHE_NAME = 'timebook-v2'
+const CACHE_NAME = 'timebook-v3'
+const STATIC_ASSETS = [
+  '/manifest.json',
+  '/logo192.png',
+  '/logo512.png',
+]
 
 self.addEventListener('install', event => {
   self.skipWaiting()
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => {
+      return cache.addAll(STATIC_ASSETS)
+    })
+  )
 })
 
 self.addEventListener('activate', event => {
   event.waitUntil(
     caches.keys().then(keys => {
       return Promise.all(
-        keys.map(key => caches.delete(key))
+        keys.filter(key => key !== CACHE_NAME).map(key => caches.delete(key))
       )
     })
   )
 })
 
 self.addEventListener('fetch', event => {
-  event.respondWith(fetch(event.request))
+  if (event.request.method !== 'GET') return
+  event.respondWith(
+    caches.match(event.request).then(cached => {
+      return cached || fetch(event.request).then(response => {
+        return response
+      })
+    })
+  )
 })
