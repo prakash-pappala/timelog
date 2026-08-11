e3239e6774d6cddd24da4a4a4431a83c619ed794
prakash <prakashsenior3@gmail.com>
2026-08-11 12:06:40 -0500

    register service worker in index.jsx

--- frontend/src/index.jsx
+++ frontend/src/index.jsx
@@ -8,3 +8,11 @@ root.render(
     <App />
   </React.StrictMode>
 );
+
+if ('serviceWorker' in navigator) {
+  window.addEventListener('load', () => {
+    navigator.serviceWorker.register('/sw.js')
+      .then(reg => console.log('Service Worker registered'))
+      .catch(err => console.log('Service Worker error:', err))
+  })
+}
