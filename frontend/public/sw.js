5d7cdc9ab90fb3718aaeaeef587f1245c9b6615a
prakash <prakashsenior3@gmail.com>
2026-08-11 12:53:22 -0500

    fix service worker caching

--- frontend/public/sw.js
+++ frontend/public/sw.js
@@ -1,32 +1,19 @@
-const CACHE_NAME = 'timebook-v1'
-const STATIC_ASSETS = [
-  '/',
-  '/index.html',
-  '/manifest.json',
-]
+const CACHE_NAME = 'timebook-v2'
 
 self.addEventListener('install', event => {
-  event.waitUntil(
-    caches.open(CACHE_NAME).then(cache => {
-      return cache.addAll(STATIC_ASSETS)
-    })
-  )
+  self.skipWaiting()
 })
 
 self.addEventListener('activate', event => {
   event.waitUntil(
     caches.keys().then(keys => {
       return Promise.all(
-        keys.filter(key => key !== CACHE_NAME).map(key => caches.delete(key))
+        keys.map(key => caches.delete(key))
       )
     })
   )
 })
 
 self.addEventListener('fetch', event => {
-  event.respondWith(
-    caches.match(event.request).then(cached => {
-      return cached || fetch(event.request)
-    })
-  )
+  event.respondWith(fetch(event.request))
 })
